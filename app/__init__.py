e05ac59f90ee70505e17a7528660aa140ff27cae
James Gerraty <jrgerraty@waimea.school.nz>
2025-05-13 09:54:27 +1200

    roots

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return render_template("pages/home.jinja")
+
+
+@app.get("/test/")
+def test():
+    return render_template("pages/test.jinja")
+
+@app.get("/about/")
+def about():
+    return render_template("pages/about.jinja")
